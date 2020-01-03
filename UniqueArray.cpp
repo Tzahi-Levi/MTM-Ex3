5806b31b8647792495087b3ad3c51939cbbff0ef
Tzahi Levi <tazachil@gmail.com>
2020-01-03 13:27:45 +0200

    I was create few more methods and updated some more

--- UniqueArray.cpp
+++ UniqueArray.cpp
@@ -7,6 +7,7 @@ class UniqueArray {
     int nextIndex;
     int currentNumberOfElements;
 
+    //TODO Adjust the Constructor to a generic Element and not only for int.
     UniqueArray(unsigned int size==1) : size(size), nextIndex(0), currentNumberOfElements(0) {
         data = new int[size];
         for (int i=0; i<size ; i++){
@@ -14,6 +15,7 @@ class UniqueArray {
         }
     }
 
+    //TODO Adjust the Constructor to a generic Element and not only for int.
     UniqueArray(const UniqueArray& other) : size(other.size), nextIndex(other.nextIndex),
                                             currentNumberOfElements(other.currentNumberOfElements),
                                             data(new int[other.size]) {
@@ -22,11 +24,14 @@ class UniqueArray {
         }
     }
 
+    //TODO Adjust the Destructor to a generic Element and not only for int.
     ~UniqueArray(){
         delete[] data;
     }
 
     unsigned int insert(const Element& element){
+         //TODO add Exception in case UniqueArray is full.
+
         for (int i=0; i<nextIndex ; i++) {
             if (data[i] == elemet or data[i] == 0) {
                 currentNumberOfElements++;
@@ -36,6 +41,7 @@ class UniqueArray {
             currentNumberOfElements++;
             return nextIndex++;
     }
+
     bool getIndex(const Element& element, unsigned int& index) const{
         for (int i=0; i<nextIndex ; i++) {
             if (data[i] == element) {
@@ -45,9 +51,19 @@ class UniqueArray {
         }
             return false;
     }
+
+    const Element* operator[](const Element& element) const {
+        int tempIndex;
+        if(getIndex(element, tempIndex)){
+            return *data[tempIndex];
+        }
+        return  NULL;
+    }
+
     unsigned int count() const{
         return currentNumberOfElements;
     }
+
     unsigned int size() const{
         return size;
     }
